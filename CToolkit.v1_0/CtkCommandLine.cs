31a658e135c566900cffe75518d920a37ccc63cf
呆奇士 チ <tsaiyuchi88@gmail.com>
2019-03-27 21:45:25 +0800

    bat commit

---
+++ CToolkit.v1_0/CtkCommandLine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CToolkit.v1_0
+{
+    public class CtkCommandLine
+    {
+
+
+
+        public void CmdWrite(string msg, params object[] obj)
+        {
+            if (msg != null)
+            {
+                Console.WriteLine();
+                Console.WriteLine(msg, obj);
+            }
+            Console.Write(">");
+        }
+
+        public void CommandLine(Action<string> act = null)
+        {
+            CmdWrite(this.GetType().Name);
+            var cmd = "";
+            do
+            {
+                cmd = Console.ReadLine();
+                if (act != null) act(cmd);
+
+            } while (string.Compare(cmd, "exit", true) != 0);
+
+
+        }
+
+    }
+}
