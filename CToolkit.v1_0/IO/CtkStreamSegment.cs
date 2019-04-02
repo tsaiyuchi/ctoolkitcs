8a889c4601d8b29f7b56bea5a854ea002e7b6329
呆奇士 チ <tsaiyuchi88@gmail.com>
2019-04-03 00:43:55 +0800

    bat commit

---
+++ CToolkit.v1_0/IO/CtkStreamSegment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CToolkit.v1_0.IO
+{
+    public class CtkStreamSegment : Stream
+    {
+        protected CtkStreamSegmentBuffer segBuffer;
+
+
+
+        #region Override
+
+        public override bool CanRead => throw new NotImplementedException();
+
+        public override bool CanSeek => throw new NotImplementedException();
+
+        public override bool CanWrite => throw new NotImplementedException();
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get; set; }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (this.segBuffer.IsEnd(Position)) return 0;
+            var isInSegment = this.segBuffer.IsInSegment(this.Position, count);
+
+            if (!isInSegment)
+            {
+                //Do something to get data bytes
+            }
+
+
+            var avalible = this.segBuffer.GetAvailableCount(this.Position);
+            if (avalible < 0) throw new InvalidOperationException("Search bytes is out of range");
+            if (avalible == 0) return 0;//當前無資料可讀
+
+            var cnt = this.segBuffer.Read(buffer, offset, count, this.Position);
+
+            return cnt;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ CToolkit.v1_0/IO/CtkStreamSegmentBuffer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CToolkit.v1_0.IO
+{
+    public class CtkStreamSegmentBuffer
+    {
+
+        public byte[] Buffer;
+        public int BufferValidOffset = 0;
+        public int BufferValidLength = -1;
+
+        public long AbsPosition = 0;
+        public long AbsLength = -1;
+
+        public long AbsValidEndPosition { get { return this.AbsPosition + this.BufferValidLength; } }
+
+
+        public int Read(byte[] buffer, int offset, int count, long position)
+        {
+            var start = (position - this.AbsPosition) + this.BufferValidOffset;//應開始 in buffer
+            var end = start + count;//應結束 in buffer
+            if (end >= this.BufferValidOffset + this.BufferValidLength) throw new InvalidOperationException("Cannot over valid range");
+
+            Array.Copy(this.Buffer, start, buffer, offset, count);
+
+            return 0;
+        }
+
+
+
+
+        public int GetAvailableCount(long position)
+        {
+            if (position < this.AbsPosition) return -1;//無法取得
+            var cnt = (int)(this.AbsValidEndPosition - position);
+            return cnt;
+        }
+
+        public bool IsEnd(long position) { return position >= this.AbsPosition; }
+
+        public bool IsInSegment(long position, long count)
+        {
+            //開頭比資料區間小
+            if (position < this.AbsPosition) return false;
+
+            //結束比資料區間大
+            var endPos = position + count;
+            if (endPos >= this.AbsLength) endPos = this.AbsLength - 1;
+
+            var validEndPos = this.AbsValidEndPosition;
+            //if (validEndPos >= this.AbsLength) validEndPos = this.AbsLength - 1;//有效資料不應超過資料Length
+
+            if (endPos >= validEndPos) return false;
+
+            return true;
+        }
+
+
+    }
+}
