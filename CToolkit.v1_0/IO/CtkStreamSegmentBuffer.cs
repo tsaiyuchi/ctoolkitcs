55937a5d85091ff1a797939af9796c4ce389af4a
tsaiyuchi <tsaiyuchi88@gmail.com>
2019-04-03 10:35:03 +0800

    Update CtkStreamSegmentBuffer.cs

--- CToolkit.v1_0/IO/CtkStreamSegmentBuffer.cs
+++ CToolkit.v1_0/IO/CtkStreamSegmentBuffer.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace CToolkit.v1_0.IO
 {
+    [Serializable]
     public class CtkStreamSegmentBuffer
     {
 
