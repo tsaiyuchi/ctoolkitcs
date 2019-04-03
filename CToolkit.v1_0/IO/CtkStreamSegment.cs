47235a9895b75555f194b34485fdf3f6f1be6a2c
tsaiyuchi <tsaiyuchi88@gmail.com>
2019-04-03 08:46:47 +0800

    Update CtkStreamSegment.cs

--- CToolkit.v1_0/IO/CtkStreamSegment.cs
+++ CToolkit.v1_0/IO/CtkStreamSegment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,20 +14,17 @@ namespace CToolkit.v1_0.IO
 
         #region Override
 
-        public override bool CanRead => throw new NotImplementedException();
+        public override bool CanRead { get { throw new NotImplementedException(); } }
 
-        public override bool CanSeek => throw new NotImplementedException();
+        public override bool CanSeek { get { throw new NotImplementedException(); } }
 
-        public override bool CanWrite => throw new NotImplementedException();
+        public override bool CanWrite { get { throw new NotImplementedException(); } }
 
-        public override long Length => throw new NotImplementedException();
+        public override long Length { get { throw new NotImplementedException(); } }
 
         public override long Position { get; set; }
 
-        public override void Flush()
-        {
-            throw new NotImplementedException();
-        }
+        public override void Flush() { throw new NotImplementedException(); }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
@@ -49,20 +46,12 @@ namespace CToolkit.v1_0.IO
             return cnt;
         }
 
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotImplementedException();
-        }
+        public override long Seek(long offset, SeekOrigin origin) { throw new NotImplementedException(); }
 
-        public override void SetLength(long value)
-        {
-            throw new NotImplementedException();
-        }
+        public override void SetLength(long value) { throw new NotImplementedException(); }
 
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            throw new NotImplementedException();
-        }
+        public override void Write(byte[] buffer, int offset, int count) { throw new NotImplementedException(); }
+
 
         #endregion
 
