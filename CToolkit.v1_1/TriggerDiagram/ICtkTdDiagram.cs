2de8240de501ad20c14d31f4ce10ce07f71aab8c
呆奇士 チ <tsaiyuchi88@gmail.com>
2021-08-24 23:42:27 +0800

    uniform refactor

--- CToolkit.v1_1/TriggerDiagram/ICtkTdDiagram.cs
+++ CToolkit.v1_1/TriggerDiagram/ICtkTdDiagram.cs
@@ -8,5 +8,7 @@ namespace CToolkit.v1_1.TriggerDiagram
     public interface ICtkTdDiagram : ICtkTdBlock
     {
         /*[d20210531] 標註為 Diagram 無實際作用*/
+        /*[d20210822] 基本上也不再使用*/
+
     }
 }
