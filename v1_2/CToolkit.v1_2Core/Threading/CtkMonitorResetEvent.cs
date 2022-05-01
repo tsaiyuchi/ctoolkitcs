41835395ca1abf5291e2b637df36d8ed9887cae4
呆奇士 チ <tsaiyuchi@msn.com>
2022-05-02 01:00:25 +0800

    uniform refactor

--- v1_2/CToolkit.v1_2Core/Threading/CtkMonitorResetEvent.cs
+++ v1_2/CToolkit.v1_2Core/Threading/CtkMonitorResetEvent.cs
@@ -7,44 +7,129 @@ using System.Threading.Tasks;
 
 namespace CToolkit.v1_2Core.Threading
 {
-    public class CtkMonitorResetEvent
+    public class CtkMonitorResetEvent : IDisposable
     {
-        protected ManualResetEvent Mre = new ManualResetEvent(false);
+        /*[d20220424] Monitor.Xxx 盡量不要卡太久.
+         * 一是影響效能, 二是容易有死鎖.
+         * 因此利用 Monitor 去 Set/Reset Event
+         */
+        /*[d20220501] 
+         * 若使用ManualResetEvent, 才需要搭配Monitor去阻擋同時通過兩個以上.
+         * 若使用AutoResetEvent, 本身就只會放行一個, 不用搭配Monitor. 若以防萬一, 還是可用一下Monitor
+         */
+        /*[d20220502]
+         * 若使用ManualResetEvent, WaitOne以後要自己Reset, 避免後面Thread跟著過去.
+         * 若使用AutoResetEvent, WaitOne以後會自動Reset, 只放行一個
+         */
 
 
-        public bool WaitOne()
+
+        /// <summary> 使用AutoResetEvent, 放了一個Thread後會自動再堵起來 </summary>
+        protected AutoResetEvent resetEvent = new AutoResetEvent(true);
+        public virtual CtkMonitorResetEventLocker OnceLocker()
         {
-            return this.Mre.WaitOne();
+            if (!this.TryEnter()) return null;
+            return new CtkMonitorResetEventLocker(this);
         }
-        public bool WaitOne(int millisecondsTimeout)
+
+        public virtual bool TryEnter(int waitTimeMs = 0, int tryGetLockTimeMs = 10, int tryResetTimeMs = 10, int restTimeMs = 10)
         {
-            return this.Mre.WaitOne(millisecondsTimeout);
-        }
+            var entryTime = DateTime.Now;
 
-
-
-        public bool TryEnterWaitReset(int monitorMillisecond = 0, int waitMillisecondsTimeout = 10)
-        {
-            try
+            //waitTimeMs <= 0 代表無限等待
+            while (waitTimeMs <= 0 || (DateTime.Now - entryTime).TotalMilliseconds < waitTimeMs)
             {
-                if (!Monitor.TryEnter(this, monitorMillisecond)) return false;//進不去先離開
-                if (!this.Mre.WaitOne(waitMillisecondsTimeout)) return false;//有人處理中, 先離開
-                this.Mre.Reset();//先卡住, 不讓後面的再次進行
+                try
+                {//不做大範圍Lock, 只對關鍵的
+                    var isGetLock = false;
+                    if (tryGetLockTimeMs > 0)
+                        isGetLock = Monitor.TryEnter(this, tryGetLockTimeMs);
+                    else
+                        isGetLock = Monitor.TryEnter(this);
+
+
+                    if (isGetLock)
+                    {
+                        if (tryResetTimeMs <= 0)
+                            throw new ArgumentException("if you can get locker then another cannot, reset time <= 0 will cause dead lock");
+
+                        if (this.resetEvent.WaitOne(tryResetTimeMs))
+                        {
+                            //AutoResetEvent會自動Reset, 此行不一定要執行, 只是以防萬一
+                            this.resetEvent.Reset();//先堵住, 不讓後面的再次進行
+                            return true;
+                        }
+                    }
+                }
+                finally { if (Monitor.IsEntered(this)) Monitor.Exit(this); }
+                //解鎖後再等待
+                Thread.Sleep(restTimeMs);
             }
-            finally { if (Monitor.IsEntered(this)) Monitor.Exit(this); }
-            return true;
+            return false;
+        }
+        public virtual bool TryExit(int waitTimeMs = 0, int tryGetLockTimeMs = 10)
+        {
+            var entryTime = DateTime.Now;
+
+            //waitTimeMs <= 0 代表無限等待
+            while (waitTimeMs <= 0 || (DateTime.Now - entryTime).TotalMilliseconds < waitTimeMs)
+            {
+                try
+                {
+                    //不做大範圍Lock, 只對關鍵的
+                    var isGetLock = false;
+                    if (tryGetLockTimeMs > 0)
+                        isGetLock = Monitor.TryEnter(this, tryGetLockTimeMs);
+                    else
+                        isGetLock = Monitor.TryEnter(this);
+
+                    if (isGetLock)
+                        this.resetEvent.Set();
+                }
+                finally { if (Monitor.IsEntered(this)) Monitor.Exit(this); }
+            }
+            return false;
         }
 
-        public bool TryEnterSet(int monitorMillisecond = 0)
+
+
+        #region IDisposable
+
+        // Flag: Has Dispose already been called?
+        protected bool disposed = false;
+        // Public implementation of Dispose pattern callable by consumers.
+        public virtual void Dispose()
         {
-            try
-            {
-                if (!Monitor.TryEnter(this, monitorMillisecond)) return false;//進不去先離開
-                this.Mre.Set();
-            }
-            finally { if (Monitor.IsEntered(this)) Monitor.Exit(this); }
-            return true;
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+            }
+
+            // Free any unmanaged objects here.
+            //
+            this.DisposeSelf();
+            disposed = true;
+        }
+
+        protected virtual void DisposeSelf()
+        {
+            if (this.resetEvent != null)
+                this.resetEvent.Dispose();
+        }
+
+        #endregion
+
+
 
 
     }
---
+++ v1_2/CToolkit.v1_2Core/Threading/CtkMonitorResetEventLocker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CToolkit.v1_2Core.Threading
+{
+    public class CtkMonitorResetEventLocker : IDisposable
+    {
+
+        protected CtkMonitorResetEvent mre;
+        public CtkMonitorResetEventLocker(CtkMonitorResetEvent mre)
+        {
+            this.mre = mre;
+        }
+
+
+        public void Close()
+        {
+            this.mre.TryExit();
+        }
+
+
+
+
+
+
+
+        #region IDisposable
+
+        // Flag: Has Dispose already been called?
+        protected bool disposed = false;
+        // Public implementation of Dispose pattern callable by consumers.
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+            }
+
+            // Free any unmanaged objects here.
+            //
+            this.DisposeSelf();
+            disposed = true;
+        }
+
+        protected virtual void DisposeSelf()
+        {
+            this.Close();
+        }
+
+        #endregion
+
+    }
+
+
+
+
+
+}
